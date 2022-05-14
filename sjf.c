05a6eb07e5c0c5d29a3704ac70000d8f6bbc05bc
naeem <naeemislamjr182@gmail.com>
2022-05-13 23:09:11 -0700

    sfj algo with babble&sorting

--- sjf.c
+++ sjf.c
@@ -1,3 +1,4 @@
+//using selection  soort
 /*#include<stdio.h>
  int main()
 {
@@ -13,7 +14,7 @@
         scanf("%d",&bt[i]);
         p[i]=i+1;
     }
-   //sorting of burst times
+   //sorting of burst times with selection sort
     for(i=0;i<n;i++)
     {
         pos=i;
@@ -49,103 +50,95 @@
     avg_tat=(float)total/n;
     printf("\n\nAverage Waiting Time=%f",avg_wt);
     printf("\nAverage Turnaround Time=%f\n",avg_tat);
+     printf("\n");
+    printf("\nThe grand chart : \n");
+    printf("|   ");
+    for (i = 0; i < n; i++)
+    {
+        printf("P%d   |   ",p[i]);
+    }
+    printf("\n0\t ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d \t  ",wt[i]+bt[i]);
+    }
+    printf("\n");
+    return 0;
 }*/
-
+//using babble sort
 #include<stdio.h>
-int main()
+#include<conio.h>
+#define max 30
+void main()
 {
-int i,n,p[10]={1,2,3,4,5,6,7,8,9,10},min,k=1,btime=0;
-int bt[10],temp,j,at[10],wt[10],tt[10],ta=0,sum=0;
-float wavg=0,tavg=0,tsum=0,wsum=0;
-printf(" -------Shortest Job First Scheduling ( NP )-------\n");
-printf("\nEnter the No. of processes :");
-scanf("%d",&n);
+   int i,j,n,t,p[max],bt[max],wt[max],tat[max];
+   float awt=0,atat=0;
+   //clrscr();
+   printf("Enter the number of process:");
+   scanf("%d",&n);
+   /*printf("Enter the process number");
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&p[i]);
 
-for(i=0;i<n;i++)
-{
-printf("\tEnter the burst time of %d process :",i+1);
-scanf(" %d",&bt[i]);
-printf("\tEnter the arrival time of %d process :",i+1);
-scanf(" %d",&at[i]);
-}
+   }*/
+   printf("Enter the burst time of the process:");
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&bt[i]);
+   }
+   //Applyling bubble sort here to sort according their burst time
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(bt[j]>bt[j+1])
+            {
+                t=bt[j];
+                bt[j]=bt[j+1];
+                bt[j+1]=t;
 
-/*Sorting According to Arrival Time*/
+                t=p[j];
+                p[j]=p[j+1];
+                p[j+1]=t;
 
-for(i=0;i<n;i++)
-{
-for(j=0;j<n;j++)
-{
-if(at[i]<at[j])
-{
-temp=p[j];
-p[j]=p[i];
-p[i]=temp;
-temp=at[j];
-at[j]=at[i];
-at[i]=temp;
-temp=bt[j];
-bt[j]=bt[i];
-bt[i]=temp;
-}
-}
-}
-
-/*Arranging the table according to Burst time,
-Execution time and Arrival Time
-Arrival time <= Execution time
-*/
-
-for(j=0;j<n;j++)
-{
-btime=btime+bt[j];
-min=bt[k];
-for(i=k;i<n;i++)
-{
-if (btime>=at[i] && bt[i]<min)
-{
-temp=p[k];
-p[k]=p[i];
-p[i]=temp;
-temp=at[k];
-at[k]=at[i];
-at[i]=temp;
-temp=bt[k];
-bt[k]=bt[i];
-bt[i]=temp;
-}
-}
-k++;
-}
-wt[0]=0;
-for(i=1;i<n;i++)
-{
-sum=sum+bt[i-1];
-wt[i]=sum-at[i];
-wsum=wsum+wt[i];
-}
-
-wavg=(wsum/n);
-for(i=0;i<n;i++)
-{
-ta=ta+bt[i];
-tt[i]=ta-at[i];
-tsum=tsum+tt[i];
-}
-
-tavg=(tsum/n);
-
-printf("************************");
-printf("\n RESULT:-");
-printf("\nProcess\t Burst\t Arrival\t Waiting\t Turn-around" );
-for(i=0;i<n;i++)
-{
-printf("\n p%d\t %d\t %d\t\t %d\t\t\t%d",p[i],bt[i],at[i],wt[i],tt[i]);
-}
-
-printf("\n\nAVERAGE WAITING TIME : %f",wavg);
-printf("\nAVERAGE TURN AROUND TIME : %f",tavg);
-return 0;
-}
+            }
+        }
+    }
+    printf("Process \t   burst time \t waiting time \t turn around time\n");
+    for(i=0;i<n;i++)
+    {
+        wt[i]=0;
+        tat[i]=0;
+        for(j=0;j<i;j++)
+        {
+            wt[i]=wt[i]+bt[j];
+        }
+        tat[i]=wt[i]+bt[i];
+        awt=awt+wt[i];
+        atat=atat+tat[i];
+        printf("%d\t\t\t %d\t\t %d\t\t %d\n",p[i],bt[i],wt[i],tat[i]);
+    }
+    awt=awt/n;
+    atat=atat/n;
+    printf("Average waiting time=%f\n",awt);
+    printf("Average turn around time=%f\n",atat);
+    printf("\n");
+    printf("\nThe grand chart : \n");
+    printf("|   ");
+    for (i = 0; i < n; i++)
+    {
+        printf("P%d   |   ",p[i]);
+    }
+    printf("\n0\t ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d \t  ",wt[i]+bt[i]);
+    }
+    printf("\n");
+    return 0;
+    getch();
 
 
+}
 
